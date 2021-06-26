3649c5be447f0e83d3b86aee17928f17336810f8
yancey <yancey.163.com>
2021-06-26 16:45:34 +0800

    添加了日志系统！

--- main.py
+++ main.py
@@ -7,7 +7,8 @@
 import os
 import sys
 from scrapy.cmdline import execute
+import logging
 
-
+logging.basicConfig(filename='log.txt',level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(['scrapy', 'crawl', 'soulmate'])
